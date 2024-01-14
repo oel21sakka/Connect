19440fc666c8da49273ca2c25c22d5de1b3f773e
oel21sakka <oel21sakka@gmail.com>
2024-01-14 21:09:11 +0200

    chore(repo): Remove deprecated 'post' app
    - Deleted the obsolete 'post' app from the repository after renaming it to 'blog'

--- post/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import PostView,SinglePostView
-
-urlpatterns = [
-    path('', PostView.as_view()),
-    path('<slug:slug>/<str:publish_date>/', SinglePostView.as_view(), name='SinglePostView')
-]
